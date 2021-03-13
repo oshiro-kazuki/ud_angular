cdd3acb4ad077fba7ac08ede22d6e1b379e19ff8
oshiro-kazuki <ka.oo1213mi@gmail.com>
2021-03-13 21:03:14 +0900

    node api接続

---
+++ app/server/dummy-db.js
@@ -0,0 +1,51 @@
+// const ProductShema = require("./models/product");
+
+class DummyDb {
+  constructor() {
+    this.products = [
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name      : 'iPhoneX',
+        price     : 1000,
+        desc      : 'iPhoneXiPhoneXiPhoneXiPhoneXiPhoneX',
+        heading1  : 'sample text1',
+        heading2  : 'sample text2',
+        heading3  : 'sample text3',
+        headText1 : 'sample text1sample text1sample text1sample text1',
+        headText2 : 'sample text2sample text2sample text2sample text2',
+        headText3 : 'sample text3sample text3sample text3sample text3'
+      },
+      {
+        coverImage: 'data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==',
+        name      : 'iPhone12',
+        price     : 1200,
+        desc      : 'iPhone12iPhone12iPhone12iPhone12iPhone12',
+        heading1  : 'sample text1',
+        heading2  : 'sample text2',
+        heading3  : 'sample text3',
+        headText1 : 'sample text1sample text1sample text1sample text1',
+        headText2 : 'sample text2sample text2sample text2sample text2',
+        headText3 : 'sample text3sample text3sample text3sample text3'
+      },
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name      : 'iPhone11',
+        price     : 1100,
+        desc      : 'iPhone11iPhone11iPhone11iPhone11iPhone11',
+        heading1  : 'sample text1',
+        heading2  : 'sample text2',
+        heading3  : 'sample text3',
+        headText1 : 'sample text1sample text1sample text1sample text1',
+        headText2 : 'sample text2sample text2sample text2sample text2',
+        headText3 : 'sample text3sample text3sample text3sample text3'
+      },
+    ]
+  }
+
+  pushProducts() {
+    this.products.forEach(product => {
+      newProduct.save();
+    });
+  }
+}
+module.exports = DummyDb;
